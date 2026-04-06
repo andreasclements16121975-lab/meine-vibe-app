64be9cb313e7dc6fa7c2ac8f7c785f01b76caeb6
andreasclements16121975-lab <andreasclements16121975@gmail.com>
2026-04-06 11:27:58 +0200

    Remove autocomplete listener from app.js
    Removed autocomplete listener and related code for place selection.

--- public/app.js
+++ public/app.js
@@ -8,19 +8,3 @@ const autocomplete = new google.maps.places.Autocomplete(opponentInput, {
 types: ['establishment', 'geocode'],
 componentRestrictions: { country: 'de' }
 });
-
-autocomplete.addListener('place_changed', () => {
-const place = autocomplete.getPlace();
-
-```
-opponentInput.value = place.name || '';
-
-if (addressInput) {
-  addressInput.value = place.formatted_address || '';
-}
-
-console.log('Ausgewählt:', place.name, place.formatted_address);
-```
-
-});
-}
