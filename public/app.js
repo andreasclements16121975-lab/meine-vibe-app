adee520479aff22de57e790645c07b2de4278a53
andreasclements16121975-lab <andreasclements16121975@gmail.com>
2026-04-06 11:07:52 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,15 @@
 function initAutocomplete() {
-  const input = document.getElementById('eventOpponent');
-  if (input) {
-    new google.maps.places.Autocomplete(input, { types: ['establishment', 'geocode'] });
-  }
+const input = document.getElementById('eventOpponent');
+if (!input) return;
+
+const autocomplete = new google.maps.places.Autocomplete(input, {
+types: ['establishment'],
+componentRestrictions: { country: 'de' }
+});
+
+autocomplete.addListener('place_changed', () => {
+const place = autocomplete.getPlace();
+console.log('Ausgewählter Gegner:', place.name, place.formatted_address);
+input.value = place.name || '';
+});
 }
